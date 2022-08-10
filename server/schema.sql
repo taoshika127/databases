f9a59e69489e350871c73a6bbe7c17e097d77aa6
bbissing <bbissing101@gmail.com>
2022-08-09 20:25:50 -0500

    Complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -4,10 +4,20 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id INT unsigned NOT NULL AUTO_INCREMENT,
+  username VARCHAR(150) NOT NULL,
+  roomname VARCHAR(150) NOT NULL,
+  text VARCHAR(150) NOT NULL,
+  PRIMARY KEY (id)
+
 );
 
 /* Create other tables and define schemas for them here! */
-
+CREATE TABLE users (
+  id INT unsigned NOT NULL AUTO_INCREMENT,
+  username VARCHAR(150) NOT NULL,
+  PRIMARY KEY (id)
+);
 
 
 
